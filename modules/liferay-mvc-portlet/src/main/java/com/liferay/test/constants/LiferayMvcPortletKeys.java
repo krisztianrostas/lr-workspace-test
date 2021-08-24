f75dee8dea333349ccec930276cb237e9c4be0c8
krisztianrostas <krisztian.rostas@liferay.com>
2021-08-24 13:58:58 +0200

    Liferay MVC portlet added

---
+++ modules/liferay-mvc-portlet/src/main/java/com/liferay/test/constants/LiferayMvcPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.test.constants;
+
+/**
+ * @author rostaskrisztian
+ */
+public class LiferayMvcPortletKeys {
+
+	public static final String LIFERAYMVC =
+		"com_liferay_test_LiferayMvcPortlet";
+
+}
---
+++ modules/liferay-mvc-portlet/src/main/java/com/liferay/test/portlet/LiferayMvcPortlet.java
@@ -0,0 +1,30 @@
+package com.liferay.test.portlet;
+
+import com.liferay.test.constants.LiferayMvcPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author rostaskrisztian
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=LiferayMvc",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + LiferayMvcPortletKeys.LIFERAYMVC,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class LiferayMvcPortlet extends MVCPortlet {
+}
